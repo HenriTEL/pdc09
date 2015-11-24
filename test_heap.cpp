527766ea9eac5da554a4eefedacb2a15aa7df210
Henri HANNETEL <henri.hannetel@insa-lyon.fr>
2015-11-24 11:11:01 +0100

    work shared

--- test_heap.cpp
+++ test_heap.cpp
@@ -6,18 +6,22 @@ int main()
 {
 	// TODO
 		// StructClassSSF.h
-			// vector<uint32_t>::const_iterator /* & */ predictPtr(Sample<FeatureType> &sample) const
-			// void updateError(IErrorData &newError, const IErrorData &errorData,
-			//     const TNode<SplitData<FeatureType>, Prediction> *node, Prediction &newLeft,
-			//     Prediction &newRight) const
-			// virtual void write(const TNode<SplitData<FeatureType>, Prediction> *node, ostream &out) const
-			// virtual void read(TNode<SplitData<FeatureType>, Prediction> *node, istream &in) const
+		    // Henri
+				// vector<uint32_t>::const_iterator /* & */ predictPtr(Sample<FeatureType> &sample) const
+				// void updateError(IErrorData &newError, const IErrorData &errorData,
+				//     const TNode<SplitData<FeatureType>, Prediction> *node, Prediction &newLeft,
+				//     Prediction &newRight) const
+			// Karim
+				// virtual void write(const TNode<SplitData<FeatureType>, Prediction> *node, ostream &out) const
+				// virtual void read(TNode<SplitData<FeatureType>, Prediction> *node, istream &in) const
 		// RandomHeap.h
-			// void split(uint32_t start, uint32_t middle)
-			// Prediction predict(Sample &sample) const
-			// bool tryImprovingSplit(ErrorData &errorData, TNode<SplitData, Prediction> *node)
-			// virtual void write(const TNode<SplitData, Prediction> *node, ostream &out) const
-			// virtual void read(TNode<SplitData, Prediction> *node, istream &in) const
+			// Henri
+				// void split(uint32_t start, uint32_t middle)
+				// Prediction predict(Sample &sample) const
+			// Karim
+				// bool tryImprovingSplit(ErrorData &errorData, TNode<SplitData, Prediction> *node)
+				// virtual void write(const TNode<SplitData, Prediction> *node, ostream &out) const
+				// virtual void read(TNode<SplitData, Prediction> *node, istream &in) const
 
     StrucClassSSF<float> *forest = new StrucClassSSF<float>[10];
     //RandomTree* tree = new RandomTree();
